0d6c316621f06b78fd14a59f453b38c258c75fbe
Sasha Ostrenko <ostrenkosasha@gmail.com>
2023-10-26 00:44:55 +0300

    update

--- snake.js
+++ snake.js
@@ -1,31 +1,27 @@
 function Snake(){
-    //head coordinates
-    this.x = 0;
-    this.y = 0;
-    //
+    this.head = createVector(0, 0);
     this.directionVector = createVector(1, 0);
     this.tail = [];
 
     /*  *** UPDATE *** */
     this.update = function(){
-        this.x += this.directionVector.x*scl;
-        this.y += this.directionVector.y*scl;
-        //I set boundaries so that the snake does not go beyond the boundaries of the canvas.
-        if (this.x > width - scl){
-            this.x = 0;
-        }else if (this.y > height - scl){
-            this.y = 0;
-        }else if(this.x < 0){
-            this.x = width - scl;
-        }else if(this.y < 0){
-            this.y = height - scl;
+        this.head.add(this.directionVector.copy().mult(scl));
+        // boundaries control
+        if (this.head.x > width - scl){
+            this.head.x = 0;
+        }else if (this.head.y > height - scl){
+            this.head.y = 0;
+        }else if(this.head.x < 0){
+            this.head.x = width - scl;
+        }else if(this.head.y < 0){
+            this.head.y = height - scl;
         }
         // tail control
         this.death();
         for(var i = this.tail.length-1; i > 0 ; i--){
             this.tail[i] = this.tail[i-1];
         }
-        this.tail[0] = createVector(this.x, this.y);
+        this.tail[0] = createVector(this.head.x, this.head.y);
     }
 
     /*  *** SHOW *** */
@@ -45,7 +41,7 @@ function Snake(){
     this.eat = function(posArr){
         var d = [];
         posArr.forEach(e => {
-            d.push(dist(this.x, this.y, e.x, e.y));
+            d.push(dist(this.head.x, this.head.y, e.x, e.y));
         });
 
         for (var i = 0; i < d.length; i++){
@@ -64,11 +60,11 @@ function Snake(){
     this.death = function(){ 
         //if the current coordinates of the snake intersect with the tail, it means it crashed into itself, which means we need to clean the tail
         for(var i = 0; i<this.tail.length; i++){
-            var d = dist(this.x, this.y, this.tail[i].x, this.tail[i].y);
+            var d = dist(this.head.x, this.head.y, this.tail[i].x, this.tail[i].y);
             if (d<1){
                 this.tail = [];
-                this.x = 0;
-                this.y = 0;
+                this.head.x = 0;
+                this.head.y = 0;
             }
         }
     }
